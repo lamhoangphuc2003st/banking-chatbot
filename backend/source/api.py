d2a561852545ded4116ad9a4a6d523155839631f
Lam Hoang Phuc <lamhoangphuc2003st@gmail.com>
2026-03-12 21:38:05 +0700

    fix python module path

--- backend/source/api.py
+++ backend/source/api.py
@@ -6,7 +6,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 
-from rag_pipeline import RAGPipeline
+from source.rag_pipeline import RAGPipeline
 
 
 # -----------------------------
